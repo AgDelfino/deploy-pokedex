1c1a14fcd9303df06ad52aa75759b5dcc3adfc4d
AgDelfino <agustin.delfino92@gmail.com>
2022-12-11 20:43:06 -0300

    eigth commit

--- api/index.js
+++ api/index.js
@@ -20,10 +20,11 @@
 require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
+const {PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('%s listening at ' + process.env.PORT); // eslint-disable-line no-console
+  server.listen(PORT, () => {
+    console.log('%s listening at ' + PORT); // eslint-disable-line no-console
   });
 });
